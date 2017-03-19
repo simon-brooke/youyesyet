7439e5ae1563ad960a381678017c9dea75084b45
Simon Brooke <simon@journeyman.cc>
2017-03-19 17:54:37 +0000

    #30: Starting to work.
    We can serve both conventional pages, and the app.

--- src/clj/youyesyet/routes/home.clj
+++ src/clj/youyesyet/routes/home.clj
@@ -5,11 +5,14 @@
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 
+(defn app-page []
+  (layout/render "app.html"))
+
 (defn home-page []
-  (layout/render "home.html"))
+  (layout/render "home.html" {:title "You Yes Yet?"}))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
+  (GET "/app" [] (app-page))
   (GET "/docs" [] (-> (response/ok (-> "docs/docs.md" io/resource slurp))
                       (response/header "Content-Type" "text/plain; charset=utf-8"))))
-
---
+++ src/cljc/youyesyet/ui_utils.cljc
@@ -0,0 +1,1 @@
+;; Ultimately it's the goal to use the same UI utils for both main site and single page app.
